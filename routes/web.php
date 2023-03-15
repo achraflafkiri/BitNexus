8fbec9085ae8b0852a17639da66e4bd2e427ed9f
achraf lafkiri <lafkiri.achraf1@ofppt-edu.ma>
2023-03-15 23:53:04 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\torrentController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
@@ -18,23 +19,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $torrents = DB::table('torrents')
-    ->select('torrents.*')
-    ->orderBy("created_at", "desc")
-    ->limit(3)
-    ->get();
+        ->select('torrents.*')
+        ->orderBy("created_at", "desc")
+        ->limit(3)
+        ->get();
     return view('welcome', ["torrents" => $torrents]);
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-
+// Torrents
 Route::get('/torrents', [torrentController::class, 'index']);
-Route::get('/torrents/create', [torrentController::class, 'create']);
-Route::post('/torrents/post', [torrentController::class, 'store']);
-Route::get('/torrents/{torrent}', [torrentController::class, 'show']);
-Route::get('/torrents/{torrent}/edit', [torrentController::class, 'edit']);
-Route::put('/torrents/{torrent}', [torrentController::class, 'update']);
-Route::delete('/torrents/{torrent}', [torrentController::class, 'destroy']);
+Route::middleware([Authenticate::class])->group(function () {
+    Route::get('/torrents/create', [torrentController::class, 'create']);
+    Route::post('/torrents/post', [torrentController::class, 'store']);
+    Route::get('/torrents/{torrent}', [torrentController::class, 'show']);
+    Route::get('/torrents/{torrent}/edit', [torrentController::class, 'edit']);
+    Route::put('/torrents/{torrent}', [torrentController::class, 'update']);
+    Route::delete('/torrents/{torrent}', [torrentController::class, 'destroy']);
+});
